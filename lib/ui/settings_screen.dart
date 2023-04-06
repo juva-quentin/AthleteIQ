59cb1cbd0d0bb51be7e8c4b7068363f8ec2be44f
Célian Frasca <celian.frasca@gmail.com>
2023-04-06 15:47:23 +0200

    feature: visualisation des infos déjà connue dans les Settings

--- lib/ui/settings_screen.dart
+++ lib/ui/settings_screen.dart
@@ -1,11 +1,14 @@
+import 'package:athlete_iq/data/network/userRepository.dart';
 import 'package:athlete_iq/ui/auth/login_screen.dart';
 import 'package:athlete_iq/ui/home/home_screen.dart';
+import 'package:athlete_iq/ui/info/provider/user_provider.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:unicons/unicons.dart';
 
 import '../app/app.dart';
+import '../model/User.dart';
 import '../utils/routes/root.dart';
 import 'auth/providers/auth_view_model_provider.dart';
 
@@ -16,6 +19,7 @@ class SettingsScreen extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final auth = ref.watch(authViewModelProvider);
+    final user = ref.watch(firestoreUserProvider);
     final height = MediaQuery.of(context).size.height;
 
     final width = MediaQuery.of(context).size.width;
@@ -27,17 +31,16 @@ class SettingsScreen extends ConsumerWidget {
             style: TextStyle(fontSize: 30, fontWeight: FontWeight.w600),
           ),
           leading: IconButton(
-            icon: Icon(UniconsLine.arrow_left, size: width * .1),
-            onPressed: () =>  Navigator.pushReplacementNamed(
-                context, App.route)
-          ),
+              icon: Icon(UniconsLine.arrow_left, size: width * .1),
+              onPressed: () =>
+                  Navigator.pushReplacementNamed(context, App.route)),
           actions: [
             IconButton(
                 onPressed: () async {
                   try {
                     await auth.logout();
-                    Navigator.pushNamedAndRemoveUntil(
-                        context, LoginScreen.route, (Route<dynamic> route) => false);
+                    Navigator.pushNamedAndRemoveUntil(context,
+                        LoginScreen.route, (Route<dynamic> route) => false);
                   } catch (e) {
                     print(e);
                   }
@@ -45,6 +48,91 @@ class SettingsScreen extends ConsumerWidget {
                 icon: Icon(UniconsLine.exit, size: width * .1)),
           ],
         ),
-        body: Container());
+        body: Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+            TextFormField(
+              initialValue: user.when(
+                data: (user) {
+                  return user.pseudo;
+                },
+                error: (Object error, StackTrace? stackTrace) {
+                  "Erreur";
+                },
+                loading: () {
+                  "Chargement...";
+                },
+              ),
+              autocorrect: false,
+              textCapitalization: TextCapitalization.none,
+              textInputAction: TextInputAction.next,
+              decoration: const InputDecoration(
+                prefixIcon: Icon(Icons.person),
+                labelText: "Pseudo",
+              ),
+              onChanged: (v) => auth.pseudo = v,
+            ),
+            TextFormField(
+              initialValue: user.when(
+                data: (user) {
+                  return user.email;
+                },
+                error: (Object error, StackTrace? stackTrace) {
+                  "Erreur";
+                },
+                loading: () {
+                  "Chargement...";
+                },
+              ),
+              autocorrect: false,
+              textCapitalization: TextCapitalization.none,
+              textInputAction: TextInputAction.next,
+              decoration: const InputDecoration(
+                prefixIcon: Icon(Icons.email),
+                labelText: "Email",
+              ),
+              onChanged: (v) => auth.email = v,
+            ),
+            TextFormField(
+              initialValue: user.when(
+                data: (user) {
+                  return user.objectif.toString();
+                },
+                error: (Object error, StackTrace? stackTrace) {
+                  "Erreur";
+                },
+                loading: () {
+                  "Chargement...";
+                },
+              ),
+              autocorrect: false,
+              textCapitalization: TextCapitalization.none,
+              textInputAction: TextInputAction.next,
+              decoration: const InputDecoration(
+                prefixIcon: Icon(Icons.bike_scooter),
+                labelText: "Objectif",
+              ),
+              onChanged: (v) => auth.email = v,
+            ),
+            TextFormField(
+              obscureText: auth.obscurePassword,
+              keyboardType: TextInputType.visiblePassword,
+              initialValue: auth.password,
+              decoration: InputDecoration(
+                prefixIcon: const Icon(Icons.lock_outline_rounded),
+                labelText: "Mot de passe",
+                suffixIcon: IconButton(
+                  onPressed: () {
+                    auth.obscurePassword = !auth.obscurePassword;
+                  },
+                  icon: Icon(auth.obscurePassword
+                      ? Icons.visibility_off_outlined
+                      : Icons.visibility_outlined),
+                ),
+              ),
+              onChanged: (v) => auth.password = v,
+            )
+          ]),
+        ));
   }
 }
