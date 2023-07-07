86cfbd34b9103f285d173fadc017b785db4b1110
juva <juvet.quentin19@gmail.com>
2023-07-07 17:46:57 +0200

    fix onbording redirection

--- lib/ui/providers/cache_provider.dart
+++ lib/ui/providers/cache_provider.dart
@@ -1,7 +1,4 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 final cacheProvider = FutureProvider((ref)=>SharedPreferences.getInstance());
-
-final userIsConneted = FutureProvider((ref) => FirebaseAuth.instance.authStateChanges().first);
